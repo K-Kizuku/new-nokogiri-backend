2f298dcb8afb52115f4e002702626a069a5401df
K-Kizuku <kotani.kizuku@gmail.com>
2023-01-22 10:40:56 +0900

    fix

--- ws/client.go
+++ ws/client.go
@@ -119,7 +119,6 @@ func (c *Client) writePump() {
 			// Add queued chat messages to the current websocket message.
 			n := len(c.send)
 			for i := 0; i < n; i++ {
-				// w.Write(newline)
 				w.Write(<-c.send)
 			}
 
